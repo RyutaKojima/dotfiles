5eccb7ebbd32b140a42368646909a1de153485a6
Ryuta Kojima <r-kojima@RyutanoMacBook-Pro.local>
2021-09-17 22:46:11 +0900

    add asdf

--- bin/install.sh
+++ bin/install.sh
@@ -3,6 +3,7 @@ cd `dirname $0`
 
 CURRENT_DIR=`pwd`
 
+ln -fs "$CURRENT_DIR/../.asdfrc" ~/.asdfrc 
 ln -fs "$CURRENT_DIR/../.config/fish" ~/.config/fish 
 ln -fs "$CURRENT_DIR/../.config/starship.toml" ~/.config/starship.toml 
 
