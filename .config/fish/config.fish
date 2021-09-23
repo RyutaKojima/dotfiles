d681d714049ad0425b347048cc3ef2bede443588
Ryuta Kojima <r-kojima@RyutanoMacBook-Pro.local>
2021-09-23 13:41:18 +0900

    add exa

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,10 @@
 
+alias e="exa --icons --git"
+alias l="e"
+alias ls="e"
+alias lt='exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
+alias lta='exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
+
 abbr -a gg ghq get
 abbr -a g cd "(ghq list --full-path | fzf --preview='cd {} & git branch -a')"
 # abbr -a gp "cd (ghq list --full-path | fzf) && phpstorm ./"
