93bbabccdb765bd30be3e8be33528513f1732f9d
Ryuta Kojima <r-kojima@RyutanoMacBook-Pro.local>
2021-09-30 10:29:06 +0900

    add delta

--- bin/install.sh
+++ bin/install.sh
@@ -3,6 +3,7 @@ cd `dirname $0`
 
 CURRENT_DIR=`pwd`
 
+ln -fs "$CURRENT_DIR/../.gitconfig" ~/.gitconfig
 ln -fs "$CURRENT_DIR/../.asdfrc" ~/.asdfrc 
 ln -fs "$CURRENT_DIR/../.tool-versions" ~/.tool-versions 
 ln -fs "$CURRENT_DIR/../.config/fish" ~/.config/fish 
