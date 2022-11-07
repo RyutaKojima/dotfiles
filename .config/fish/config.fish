61f04eb04f65a7b2f312cc16ba1bbfdc4796997b
RyutaKojima <rkojima0213@gmail.com>
2022-11-07 13:40:48 +0900

    update: config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,28 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -x PATH /opt/homebrew/bin $PATH
 
 alias e="exa --icons --git"
 alias l="e"
 alias ls="e"
+alias ll="e -l"
+alias la="ll -a"
 alias lt='exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
 alias lta='exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
 
 abbr -a gg ghq get
 abbr -a branches "git branch --all"
 
+# Docker Compose
+abbr -a dcu docker-compose up -d
+abbr -a dce docker-compose exec app ash
+abbr -a dcd docker-compose down
+
 set -x PATH $HOME/.anyenv/bin $PATH
 
 # Setup asdf
-source /usr/local/opt/asdf/libexec/asdf.fish
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # iTerm integration
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
